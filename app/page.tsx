bd63cdef852aa3521de6ce3176accf3ae211be3f
mizdra <pp.mizdra@gmail.com>
2024-04-01 02:25:36 +0900

    use @mizdra/node-next-image-loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
+import Image from 'next/image';
+
 export default async function Home() {
-  const data = await fetch(`${process.env.API_ENDPOINT}/api/hello`);
-  return <main>{await data.text()}</main>;
+  const helloData = await fetch(`${process.env.API_ENDPOINT}/api/hello`);
+  const imageData = await fetch(`${process.env.API_ENDPOINT}/api/image`);
+  return (
+    <main>
+      <div>{await helloData.text()}</div>
+      <div>
+        <Image src={await imageData.json()} alt="JavaScript Logo" />
+      </div>
+    </main>
+  );
 }
--- lib/mocks/handlers.ts
+++ lib/mocks/handlers.ts
@@ -1,7 +1,11 @@
 import { http, HttpResponse } from 'msw';
+import I_JS from '../../assets/js.png';
 
 export const handlers = [
   http.get('/api/hello', () => {
     return HttpResponse.text(`Hello, world! (${new Date().toISOString()})`);
+  }),
+  http.get('/api/image', () => {
+    return HttpResponse.json(I_JS);
   }),
 ];
