c38d9b1687bcd3c26e777b7f44da7c94d30646b2
mizdra <pp.mizdra@gmail.com>
2024-04-14 14:38:37 +0900

    use API Routes

---
+++ pages/api/[...index].ts
@@ -0,0 +1,19 @@
+// In truth, it is simpler to use msw/node in the process of launching Next.js
+// and mocking it.However, that is known to be difficult.
+//
+// - https://github.com/mswjs/msw/issues/1644
+// - https://github.com/vercel/next.js/discussions/56446
+//
+// So, start a new Node.js process and launch a mock server that listens on a different port than Next.js.
+// Then, requests are sent from the Next.js process to this mock server.
+
+import { createServer } from '@mswjs/http-middleware';
+import { handlers } from '../../lib/mocks/handlers';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const httpServer = createServer(...handlers);
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('req.url:', req.url);
+  httpServer(req, res);
+}
