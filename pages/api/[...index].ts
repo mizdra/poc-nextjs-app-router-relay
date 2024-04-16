e963df85c795639d020e85067e94106f5508b25b
mizdra <pp.mizdra@gmail.com>
2024-04-16 23:43:15 +0900

    update comment

--- pages/api/[...index].ts
+++ pages/api/[...index].ts
@@ -1,11 +1,19 @@
-// In truth, it is simpler to use msw/node in the process of launching Next.js
-// and mocking it.However, that is known to be difficult.
+// Mock API requests with msw.
+// Usually, it is common to use `msw/node` or `msw/browser` to intercept API requests
+// and process them with handlers.
+//
+// - https://mswjs.io/docs/integrations/browser
+// - https://mswjs.io/docs/integrations/node
+//
+// `msw/node` works by monkey-patching the `globalThis.fetch` or `node:http` module.
+// In addition, Next.js also monkey patches `globalThis.fetch`. Therefore, if you use
+// `msw/node` with Next.js, their monkey patches conflict and mocking will not work.
 //
 // - https://github.com/mswjs/msw/issues/1644
 // - https://github.com/vercel/next.js/discussions/56446
 //
-// So, start a new Node.js process and launch a mock server that listens on a different port than Next.js.
-// Then, requests are sent from the Next.js process to this mock server.
+// So we mock API requests with msw instead of using `msw/node`. To do so, we combine
+// `express` with `@mswjs/http-middleware`.
 
 import { createMiddleware } from '@mswjs/http-middleware';
 import { handlers } from '../../lib/mocks/handlers';
@@ -16,6 +24,5 @@ const app = express();
 app.use(createMiddleware(...handlers));
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('req.url:', req.url);
   app(req, res);
 }
