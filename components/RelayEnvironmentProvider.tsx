40e4e501dbd9c6bd047715988730c6df48d93486
mizdra <pp.mizdra@gmail.com>
2024-04-21 19:50:25 +0900

    keep environment instance

--- components/RelayEnvironmentProvider.tsx
+++ components/RelayEnvironmentProvider.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { createEnvironment } from '@/lib/relay/environment';
+import { useMemo } from 'react';
 // biome-ignore lint/nursery/noRestrictedImports: This is CC.
 import { RelayEnvironmentProvider as OriginalRelayEnvironmentProvider } from 'react-relay';
 
 // Original `RelayEnvironmentProvider` calls `createContext` but is not marked with 'use client'.
 // Therefore, wrap it and mark the wrapped one with 'use client'.
 export function RelayEnvironmentProvider({ children }: { children: React.ReactNode }) {
-  const environment = createEnvironment();
+  const environment = useMemo(() => createEnvironment(), []);
   return <OriginalRelayEnvironmentProvider environment={environment}>{children}</OriginalRelayEnvironmentProvider>;
 }
