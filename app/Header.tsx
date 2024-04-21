e7e6512b266d80f9dcb22a6ffed9a0a164cf7175
mizdra <pp.mizdra@gmail.com>
2024-04-21 17:16:51 +0900

    fixed hydration error
    - `useLazyLoadQuery` causes "Streaming with Suspense" mode.
    - This results the query is fetched on the server, not on the client.

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,16 +1,31 @@
 'use client';
 
 import type { HeaderQuery } from '@/app/__generated__/HeaderQuery.graphql';
-import { Suspense } from 'react';
+import { useEffect } from 'react';
 // biome-ignore lint/nursery/noRestrictedImports: This is CC.
-import { useLazyLoadQuery } from 'react-relay';
+import { type PreloadedQuery, usePreloadedQuery, useQueryLoader } from 'react-relay';
 import { graphql } from 'relay-runtime';
 
 import { User } from '@/components/User';
 import Link from 'next/link';
 import styles from './Header.module.css';
 
+const headerQuery = graphql`
+  query HeaderQuery @raw_response_type {
+    viewer {
+      user {
+        ...User_user
+      }
+    }
+  }
+`;
+
 export function Header() {
+  const [queryReference, loadQuery] = useQueryLoader<HeaderQuery>(headerQuery);
+  // Fetch user-specific data on the client side to keep the entire page cacheable on the CDN.
+  useEffect(() => {
+    loadQuery({});
+  }, [loadQuery]);
   return (
     <header className={styles.header}>
       <div>
@@ -18,29 +33,13 @@ export function Header() {
           mini-blog
         </Link>
       </div>
-      <div>
-        <Suspense fallback={'Loading...'}>
-          <LoginButtonOrUser />
-        </Suspense>
-      </div>
+      <div>{queryReference ? <LoginButtonOrUser queryReference={queryReference} /> : 'Loading...'}</div>
     </header>
   );
 }
 
-function LoginButtonOrUser() {
-  // Fetch user-specific data on the client side to keep the entire page cacheable on the CDN.
-  const { viewer } = useLazyLoadQuery<HeaderQuery>(
-    graphql`
-      query HeaderQuery @raw_response_type {
-        viewer {
-        user {
-          ...User_user
-        }
-      }
-      }
-    `,
-    {},
-  );
+function LoginButtonOrUser({ queryReference }: { queryReference: PreloadedQuery<HeaderQuery> }) {
+  const { viewer } = usePreloadedQuery(headerQuery, queryReference);
   if (viewer) {
     return (
       <div className={styles.userInfo}>
